8dfb0f6ad22bb8589c31fc88082cbbc140412635
GiuPl <48204030+GiuPl@users.noreply.github.com>
2022-03-25 11:36:25 +0100

    Create init.tf

---
+++ init.tf
@@ -0,0 +1,22 @@
+provider "vault" {
+}
+
+terraform {
+required_version = ">= 0.14.0"
+  required_providers {
+    openstack = {
+      source  = "terraform-provider-openstack/openstack"
+      version = "~> 1.35.0"
+    }
+  }
+}
+
+provider "external" {
+}
+
+provider "openstack" {
+}
+
+
+provider "consul" {
+}
